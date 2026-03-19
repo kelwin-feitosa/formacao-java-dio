bf5044b84f72de9db69ec47e6698a211ad0cf0fa
Kelwin Feitosa <kelwinfeitosa@gmail.com>
2026-03-19 20:53:07 -0300

    feat: setup inicial e implementação do ContaTerminal

---
+++ src/ContaTerminal.java
@@ -0,0 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in).useLocale(Locale.US);
+        
+        System.out.println("Por favor, digite o seu Nome: ");
+        String nomeCliente = scan.nextLine();
+
+        System.out.println("Por favor, digite a Agência: ");
+        String agencia = scan.nextLine();   
+
+        System.out.println("Por favor, digite o número da Agência: ");
+        int numero = scan.nextInt();
+
+        System.out.println("Por favor, digite o Saldo: ");
+        double saldo = scan.nextDouble();
+
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e o seu saldo é de %.2f.%n", 
+                          nomeCliente, agencia, numero, saldo);
+
+    }
+}
