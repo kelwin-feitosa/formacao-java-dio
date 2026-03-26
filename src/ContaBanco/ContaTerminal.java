9cc8d53ffd4308f638d08a7a5025b2459dbb7325
Kelwin Feitosa <kelwinfeitosa@gmail.com>
2026-03-26 17:34:21 -0300

    feat: organiza projeto ContaBanco em pasta própria

---
+++ src/ContaBanco/ContaTerminal.java
@@ -0,0 +1,27 @@
+package contabanco;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in).useLocale(Locale.US);
+        
+        System.out.println("Por favor, digite o seu Nome: ");
+        String nomeCliente = scan.nextLine();
+
+        System.out.println("Por favor, digite a Agência: ");
+        String agencia = scan.nextLine();   
+
+        System.out.println("Por favor, digite o número da Agência: ");
+        int numero = scan.nextInt();
+
+        System.out.println("Por favor, digite o Saldo: ");
+        double saldo = scan.nextDouble();
+
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e o seu saldo de %.2f já está disponível para saque.%n", 
+                          nomeCliente, agencia, numero, saldo);
+        
+        scan.close();
+    }
+}
