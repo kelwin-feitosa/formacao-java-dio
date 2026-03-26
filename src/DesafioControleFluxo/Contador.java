0d56b42ea2fdf657bff5e572bb886b1f676aa178
Kelwin Feitosa <kelwinfeitosa@gmail.com>
2026-03-26 17:36:14 -0300

    feat: implementa desafio de controle de fluxo com validação de exceção

---
+++ src/DesafioControleFluxo/Contador.java
@@ -0,0 +1,34 @@
+package DesafioControleFluxo;
+
+import java.util.Scanner;
+
+public class Contador {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Digite o primeiro parâmetro");
+		int parametroUm = scan.nextInt();
+
+		System.out.println("Digite o segundo parâmetro");
+		int parametroDois = scan.nextInt();
+		
+		try {
+			contar(parametroUm, parametroDois);
+
+		}catch (ParametrosInvalidosException e) {
+            System.err.println(e.getMessage());
+		}
+		
+		scan.close();
+	}
+	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+		if(parametroUm > parametroDois) 
+			throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
+		
+		int contagem = parametroDois - parametroUm;
+
+		for(int i = 1; i <= contagem; i++) {
+			System.out.println("Imprimindo o número " + i);
+		}
+	}
+}
