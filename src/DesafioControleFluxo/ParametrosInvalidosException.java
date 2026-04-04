2f5226dec5879f0766e00c5f918e24a4adf2a866
Kelwin Feitosa <kelwinfeitosa@gmail.com>
2026-04-04 15:56:07 -0300

    removendo pastas duplicadas

--- src/DesafioControleFluxo/ParametrosInvalidosException.java
+++
@@ -1,7 +0,0 @@
-package DesafioControleFluxo;
-
-public class ParametrosInvalidosException extends Exception{
-    public ParametrosInvalidosException(String mensagem) {
-        super(mensagem);
-    }
-}
